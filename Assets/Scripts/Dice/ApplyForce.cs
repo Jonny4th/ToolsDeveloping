edc69ba4a8ec51c18f6241000c296b273a0db94a
Jonny4th <91357144+Jonny4th@users.noreply.github.com>
2022-08-23 22:20:06 +0700

    Dice Daily

--- Assets/Scripts/Dice/ApplyForce.cs
+++ Assets/Scripts/Dice/ApplyForce.cs
@@ -14,22 +14,26 @@ public class ApplyForce : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        _ApplyForce();
+        if (Input.GetKeyDown("space"))
+        {
+            _ApplyForce();
+        }
     }
 
-    private void _ApplyForce()
+    public void _ApplyForce()
     {
         Rigidbody rigidbody = GetComponent<Rigidbody>();
-        if (Input.GetKeyDown("space") & rigidbody.velocity.magnitude == 0)
+        if (rigidbody.velocity.magnitude == 0)
         {
-            rigidbody.AddForce(RandomVector3() * forceMagnitude, ForceMode.Acceleration);
+            rigidbody.AddForce(Vector3.up  * forceMagnitude, ForceMode.Acceleration);
+            rigidbody.AddTorque(RandomVector3() * forceMagnitude);
         }
     }
 
     private Vector3 RandomVector3()
     {
         float x = Random.Range(-1f,1f);
-        float y = Random.Range(0,1f);
+        float y = Random.Range(-1f,1f);
         float z = Random.Range(-1f,1f);
 
         Vector3 vector = new Vector3(x,y,z).normalized;
